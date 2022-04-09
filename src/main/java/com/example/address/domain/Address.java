d57e070c8f8620dc344f1742fedd032ac391c530
Suraj Mishra <mishrasur7@gmail.com>
2022-04-09 15:16:43 +0300

    address attributes added

--- src/main/java/com/example/address/domain/Address.java
+++ src/main/java/com/example/address/domain/Address.java
@@ -1,5 +1,13 @@
 package com.example.address.domain;
 
 public class Address {
+	
+	private int postalCode; 
+	private String street; 
+	private String city;
+	private String block; 
+	private int houseNumber;
+	private String country; 
+	
 
 }
