dfc08c26d27023ad534114ccf700a3fa6fe05853
Suraj Mishra <mishrasur7@gmail.com>
2022-04-15 21:05:37 +0300

    constructor from object added

--- src/main/java/com/example/address/domain/Address.java
+++ src/main/java/com/example/address/domain/Address.java
@@ -1,13 +1,108 @@
 package com.example.address.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+//Address entity class to save address data of friend in Finland 
+@Entity
 public class Address {
 	
-	private int postalCode; 
-	private String street; 
-	private String city;
-	private String block; 
+	@NotNull
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id; 
+	
+	private String name;  
+	private String street;
 	private int houseNumber;
-	private String country; 
+	private String block;
+	
+	@Size(min=5, max=5)
+	private int postalCode;
+	
+	private String city;
+
+	public Address() {
+		
+	}
+
+	public Address(String name, String street, int houseNumber, String block, @Size(min = 5, max = 5) int postalCode,
+			String city) {
+		super();
+		this.name = name;
+		this.street = street;
+		this.houseNumber = houseNumber;
+		this.block = block;
+		this.postalCode = postalCode;
+		this.city = city;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public int getHouseNumber() {
+		return houseNumber;
+	}
+
+	public void setHouseNumber(int houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+
+	public String getBlock() {
+		return block;
+	}
+
+	public void setBlock(String block) {
+		this.block = block;
+	}
+
+	public int getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", name=" + name + ", street=" + street + ", houseNumber=" + houseNumber
+				+ ", block=" + block + ", postalCode=" + postalCode + ", city=" + city + "]";
+	}
+	
 	
 
 }
