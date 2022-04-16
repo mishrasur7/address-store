8391b7993899b80bf2b9f55b530ff4922b212eb9
Suraj Mishra <mishrasur7@gmail.com>
2022-04-16 15:31:34 +0300

    add and save function added

--- src/main/java/com/example/address/web/AddressController.java
+++ src/main/java/com/example/address/web/AddressController.java
@@ -3,20 +3,39 @@ package com.example.address.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.address.domain.Address;
 import com.example.address.domain.AddressRepository;
 
 
 @Controller
 public class AddressController {
 	
+	//dependency injected with AddressRepository 
 	@Autowired
-	private AddressRepository repository;  
+	private AddressRepository repository;
 	
-	@RequestMapping(value= {"/"})
+	//request mapping to list all the addresses stored in database
+	@RequestMapping(value= {"/", "/addresslist"})
 	public String addressList(Model model) {
 		model.addAttribute("addresses", repository.findAll()); 
 		return "addressList"; 
+	}
+	
+	//request mapping to add address details into database
+	@RequestMapping(value="/add")
+	public String addAddress(Model model) {
+		model.addAttribute("address", new Address()); 
+		return "addAddress"; 
+	}
+	
+	//post request mapping to save address 
+	@PostMapping(value="/save")
+	public String saveAddress(Address address) {
+		repository.save(address); 
+		return "redirect:"; 
 	}
 
 }
