5550912069281cdc61acf5d949b7e8903d01c4ee
Suraj Mishra <mishrasur7@gmail.com>
2022-04-21 08:52:37 +0300

    deploy it

--- src/main/java/com/example/address/domain/User.java
+++ src/main/java/com/example/address/domain/User.java
@@ -5,9 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 //Entity class User
 @Entity
+@Table(name="usertable")
 public class User {
 	
 	//private fields with auto generated id
